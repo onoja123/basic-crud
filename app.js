f9a569fc8409a48b542f87e6d3d09888e1ef5a34
Okpe onoja <67971490+onoja123@users.noreply.github.com>
2022-11-19 22:18:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use("/test", userRoutes)
 
 app.all('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
-  });
+});
   
 
 module.exports = app;
